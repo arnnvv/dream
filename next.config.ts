008f05b86a86981b3076a3bff7b232c96a1b15ea
arnnvv <arnav.900@protonmail.com>
2024-12-20 18:15:47 +0530

    mdx

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   experimental: {
     ppr: "incremental",
   },
   serverExternalPackages: ["@node-rs/argon2"],
+  pageExtensions: ["tsx", "ts", "mdx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
